88be0760b88e9e35a164479c6c5308c5ea0f1b0d
mbllllb <enakes@gmail.com>
2017-10-16 12:28:41 +0300

    changes_for_heroku_18

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-console.log('step 1 fired!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+console.log('step 1 fired!')
 
 const express = require('express');
 const app = express();
@@ -6,24 +6,24 @@ const path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
-console.log('step 1 fired!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+console.log('step 2 fired!');
 
 // views is directory for all template files
-app.use(express.static(path.join(__dirname, 'src')))
+// app.use(express.static(path.join(__dirname, 'src')))
 // app.set('views', __dirname);
-console.log('step 1 fired!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+console.log('step 3 fired!')
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'src/index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
 })
 
-console.log('step 1 fired!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+console.log('step 4 fired!')
 
 // const server = http.createServer(app);
 // server.listen(port, () => console.log('mbllllb running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'));
 module.exports = app;
 
-console.log('step 1 fired!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+console.log('step 5 fired!')
 
 // app.get('/', function(request, response) {
 //   response.render('index');
