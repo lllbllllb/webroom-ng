1a0216c8bc3c7b44248391c2e4fa83d0ad15881b
mbllllb <enakes@gmail.com>
2017-10-16 13:47:06 +0300

    changes_for_heroku_22

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ const http = require('http');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, '')))
 
 console.log('step 1 fired!');
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 console.log('step 2 fired!');
