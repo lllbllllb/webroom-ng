f7db7d3f2b0736b319f8993ddb2aa81797684332
mbllllb <enakes@gmail.com>
2017-10-16 13:24:27 +0300

    changes_for_heroku_19

--- src/index.js
+++ src/index.js
@@ -1,34 +1,27 @@
-console.log('step 1 fired!')
+console.log('Medium mode: ')
 
 const express = require('express');
-const app = express();
 const path = require('path');
+const http = require('http');
 
-app.set('port', (process.env.PORT || 5000));
+const app = express();
+
+app.use(express.static(path.join(__ditname, 'index.html')))
+
+console.log('step 1 fired!');
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+})
 
 console.log('step 2 fired!');
 
-// views is directory for all template files
-// app.use(express.static(path.join(__dirname, 'src')))
-// app.set('views', __dirname);
+app.set('port', (process.env.PORT || 5000));
+
 console.log('step 3 fired!')
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
-})
+const server = http.createServer(app);
+server.listen(port, () => console.log('Running! (step 3b fired)'))
+// module.exports = app;
 
 console.log('step 4 fired!')
-
-// const server = http.createServer(app);
-// server.listen(port, () => console.log('mbllllb running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'));
-module.exports = app;
-
-console.log('step 5 fired!')
-
-// app.get('/', function(request, response) {
-//   response.render('index');
-// });
-
-// app.get('*', (req, res) => {
-//           res.sendFile('index.html');
-// })
