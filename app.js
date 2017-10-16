2db2168f8fcb3bbc9b99f61136f9500f838134a4
mbllllb <enakes@gmail.com>
2017-10-16 10:40:52 +0300

    changes_for_heroku_9

--- app.js
+++ app.js
@@ -1,68 +1,15 @@
 const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const compression = require('compression');
+const app = express();
 
-// var users = require('./routes/users');
-var app = express();
+app.set('port', (process.env.PORT || 5000));
 
-const options = {
-  index: "index.html"
-};
+// views is directory for all template files
+// app.set('views', __dirname);
 
-if (app.get('env') !== 'production') {
+// app.get('/', function(request, response) {
+//   response.render('index');
+// });
 
-  options.index = "index.dev.html";
-
-  // expose node_modules to client app
-  app.use(express.static(__dirname + "/node_modules"));
-}
-
-// app.use(logger('dev'));
-app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.use(express.static(path.join(__dirname, 'public'), options));
-app.use(express.static(path.join(__dirname, 'app')));
-
-// Routes registration
-// ---
-// app.use('/corpactions', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: {}
-  });
-});
-
-
-module.exports = app;
+app.get('*', (req, res) => {
+          res.sendFile('/index.html');
+})
