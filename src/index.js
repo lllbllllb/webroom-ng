3d76225beb38595c2dd01188b6e69711207ac3ef
mbllllb <enakes@gmail.com>
2017-10-16 12:12:30 +0300

    changes_for_heroku_17

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-console.log('1) mbllllb running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+console.log('step 1 fired!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
 const express = require('express');
 const app = express();
@@ -6,24 +6,24 @@ const path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
 
-console.log('2) mbllllb running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+console.log('step 1 fired!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
 // views is directory for all template files
 app.use(express.static(path.join(__dirname, 'src')))
 // app.set('views', __dirname);
-console.log('3) mbllllb running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+console.log('step 1 fired!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/index.html'));
 })
 
-console.log('4) mbllllb running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+console.log('step 1 fired!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
 // const server = http.createServer(app);
 // server.listen(port, () => console.log('mbllllb running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'));
 module.exports = app;
 
-console.log('5) mbllllb running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+console.log('step 1 fired!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
 // app.get('/', function(request, response) {
 //   response.render('index');
