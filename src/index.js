159df2cf23e8b3bd12b5a11b10b182c75c382d39
mbllllb <enakes@gmail.com>
2017-10-16 11:56:52 +0300

    changes_for_heroku_15

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
+console.log('1) mbllllb running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
+
+console.log('2) mbllllb running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
 // views is directory for all template files
 app.use(expect.static(path.join(__dirname, 'src')))
 // app.set('views', __dirname);
+console.log('3) mbllllb running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/index.html'));
 })
 
+console.log('4) mbllllb running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+
 const server = http.createServer(app);
-server.listen(port, () => console.log('mbllllb running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'))
+server.listen(port, () => console.log('mbllllb running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'));
 
 // app.get('/', function(request, response) {
 //   response.render('index');
