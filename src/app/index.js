7127ccd38572f398d773c308fcebc1233e464bc5
mbllllb <enakes@gmail.com>
2017-10-16 11:33:17 +0300

    changes_for_heroku_13

--- src/app/index.js
+++ src/app/index.js
@@ -4,12 +4,19 @@ const app = express();
 app.set('port', (process.env.PORT || 5000));
 
 // views is directory for all template files
+app.use(expect.static(path.join(__dirname, 'src')))
 // app.set('views', __dirname);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/index.html'));
+})
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('mbllllb running!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'))
 
 // app.get('/', function(request, response) {
 //   response.render('index');
 // });
 
-app.get('*', (req, res) => {
-          res.sendFile('index.html');
-})
+// app.get('*', (req, res) => {
+//           res.sendFile('index.html');
+// })
