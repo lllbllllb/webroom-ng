87fe38837ae76e9da32e26690dcba33cac600ac4
mbllllb <enakes@gmail.com>
2017-10-16 09:42:56 +0300

    changes_for_heroku_3

--- src/app/header/breadcrumb/breadcrumb.component.ts
+++ src/app/header/breadcrumb/breadcrumb.component.ts
@@ -11,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbComponent implements OnInit {
 
-  private breadCrumbsItems: MenuItem[];
+  breadCrumbsItems: MenuItem[];
 
   constructor(private eventKeeperBox: EventKeeperBoxService,
     private logger: Logger) { }
