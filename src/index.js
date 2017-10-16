6c4e8e05f5d02b35258f105631067df79978ae36
mbllllb <enakes@gmail.com>
2017-10-16 20:55:01 +0300

    v.35 heroku

--- src/index.js
+++ src/index.js
@@ -1,47 +1,39 @@
-// console.log('Medium mode: ')
+console.log('Medium mode: ')
 
-// const express = require('express');
-// const path = require('path');
-// const http = require('http');
+const express = require('express');
+const path = require('path');
+const http = require('http');
 
-// const app = express();
+const app = express();
 
-// app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static('dist'));
 
-// console.log('step 1 fired!');
+app.get('*', function (req, res) {
+  res.sendFile('index.html');
+});
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-// })
+const port = process.env.PORT || '5000';
+app.set('port', port);
 
-// console.log('step 2 fired!');
-
-// const port = process.env.PORT || '5000';
-// app.set('port', port);
-
-// console.log('step 3 fired!')
-
-// const server = http.createServer(app);
-// server.listen(port, () => console.log('Running! (step 3b fired)'))
-
-// console.log('step 4 fired!')
+const server = http.createServer(app);
+server.listen(port, () => console.log('Running! (step 3b fired)'))
 
 //
 
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+// app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/dist');
+// // views is directory for all template files
+// app.set('views', __dirname + '/dist');
 
-app.get('*', function(request, response) {
-  response.render('index.html');
-});
+// app.get('*', function(request, response) {
+//   response.render('index.html');
+// });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
